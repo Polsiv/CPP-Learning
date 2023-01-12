75fdd23caac75135fe3bae37877a833b88f06362
Silv <forenks1005@gmail.com>
2023-01-12 12:52:10 -0500

    Commiting new files

---
+++ Assert-Function/assert.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cassert>
+
+
+
+int main(int a, int b){
+
+assert(b!=0);
+
+return a/b;
+
+
+}
+
---
+++ Assert-Function/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cassert>
+
+
+//assert is for ranges
+
+int main(){
+
+int i = 5;
+int j = i * i;
+
+assert(j == 5);
+
+return 0;
+}
