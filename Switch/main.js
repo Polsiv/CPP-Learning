fc9ab4ef0f21810be3acdad68605cbe291ff11fa
Silv <91438536+Polsiv@users.noreply.github.com>
2023-01-13 18:53:18 -0500

    Delete Switch directory

--- Switch/main.js
+++
@@ -1,9 +0,0 @@
-checkbox.addEventListener('click', (event) =>{
-    if (checkbox.classList.contains('on')) {
-        checkbox.setAttribute('aria-checked', 'false');
-    } else {
-        checkbox.setAttribute('aria-checked', 'true');
-    }
-    
-    checkbox.classList.toggle('on');
-});
